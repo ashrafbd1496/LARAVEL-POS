41b7c6f04212d6df764afd504c55ea382d106bf2
ashrafuddin1496 <asraf1496@gmail.com>
2020-12-28 19:33:57 +0600

    template slicing done

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2021-2022 <a href="https://ashrafbd.com/pos">Ashrafbd</a>.</strong> All rights reserved.
+</footer>
+
+@stack('scripts')
